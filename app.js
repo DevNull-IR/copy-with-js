df51619c95985d5f8cb8a9a98c878f4e5d6e1eea
Ali <97373457+DevNull-IR@users.noreply.github.com>
2023-02-16 09:14:45 +0330

    Update app.js

--- app.js
+++ app.js
@@ -14,22 +14,27 @@ function fallbackCopyTextToClipboard(text) {
   try {
     var successful = document.execCommand('copy');
     var msg = successful ? 'successful' : 'unsuccessful';
+    var status = successful ? true : false;
     console.log('Fallback: Copying text command was ' + msg);
   } catch (err) {
+    document.body.removeChild(textArea);
     console.error('Fallback: Oops, unable to copy', err);
+    return false;
   }
 
   document.body.removeChild(textArea);
+  return status;
 }
 function copyTextToClipboard(text) {
   if (!navigator.clipboard) {
-    fallbackCopyTextToClipboard(text);
-    return;
+    return fallbackCopyTextToClipboard(text);
   }
   navigator.clipboard.writeText(text).then(function() {
     console.log('Async: Copying to clipboard was successful!');
+    return true;
   }, function(err) {
     console.error('Async: Could not copy text: ', err);
+    return false;
   });
 }
 function copy(text){
